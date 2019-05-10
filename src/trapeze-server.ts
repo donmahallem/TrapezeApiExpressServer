e5e880394565aa071c76ebd1d7318d924e1766d1
DonMahallem <donmahallem@users.noreply.github.com>
2019-05-10 09:00:58 +0200

    wip

--- src/trapeze-server.ts
+++ src/trapeze-server.ts
@@ -4,23 +4,31 @@ import * as helmet from "helmet";
 import { Server } from "http";
 import { resolve as pathResolve } from "path";
 import { IServerConfig } from "./config";
+import * as cors from "cors";
 export const api404Handler: express.RequestHandler = (req: express.Request,
-                                                      res: express.Response,
-                                                      next: express.NextFunction): void => {
+    res: express.Response,
+    next: express.NextFunction): void => {
     res.status(404).json({
         statusCode: 404,
     });
 };
 export const serverErrorHandler: express.ErrorRequestHandler = (err: any,
-                                                                req: express.Request,
-                                                                res: express.Response,
-                                                                next: express.NextFunction) => {
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction) => {
     if (err.statusCode) {
         res.status(err.statusCode).json({
             error: true,
             message: err.message,
             statusCode: err.statusCode,
         });
+        let reqUrl: string = "unknown";
+        let reqMethod: string = "unknown";
+        if (err.options) {
+            reqUrl = err.options.url;
+            reqMethod = err.options.method;
+        }
+        console.error("proxy", err.statusCode, reqMethod, reqUrl);
         return;
     }
     // tslint:disable-next-line:no-console
@@ -35,7 +43,16 @@ export class TrapezeServer {
     constructor(public readonly config: IServerConfig) {
         this.app = express();
         if (config.helmet) {
+            // tslint:disable-next-line:no-console
+            console.info("Uses helmet", config.helmet);
             this.app.use(helmet(config.helmet));
+        } else {
+            this.app.use(helmet());
+        }
+        if (config.cors) {
+            // tslint:disable-next-line:no-console
+            console.info("Uses cors", config.cors);
+            this.app.use("/api", cors(config.cors));
         }
         this.app.use("/api", createTrapezeApiRoute(config.endpoint));
         this.app.use("/api", api404Handler);
